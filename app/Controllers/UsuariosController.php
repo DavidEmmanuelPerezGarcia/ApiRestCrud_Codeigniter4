85ba17cadaed4ff4b06ab74ae1531118cc670657
DavidEmmanuelPerezGarcia <106285896+DavidEmmanuelPerezGarcia@users.noreply.github.com>
2023-06-10 18:23:32 -0600

    Se creo el metodo para traer todos los datos de los usuarios

--- app/Controllers/UsuariosController.php
+++ app/Controllers/UsuariosController.php
@@ -1,9 +1,20 @@
 <?php
 namespace App\Controllers;
+
+use App\Models\UsuariosModel;
 use CodeIgniter\API\ResponseTrait;
 use CodeIgniter\RESTful\ResourceController;
 
 class UsuariosController extends ResourceController{
     use ResponseTrait;
+
+    //metodo para mostrar todos los usuarios
+
+    public function index(){
+        $model=new UsuariosModel();
+        $datos=$model->findAll();
+
+        return $this->respond($datos,200);
+    }
 }
 ?>
