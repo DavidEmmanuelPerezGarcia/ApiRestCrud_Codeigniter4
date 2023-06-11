ce91b17725dfc24c7a3f32d8f91a6cf575dc7447
DavidEmmanuelPerezGarcia <106285896+DavidEmmanuelPerezGarcia@users.noreply.github.com>
2023-06-10 18:18:05 -0600

    Se creo el archivo del usuario controlador y se configuro

---
+++ app/Controllers/UsuariosController.php
@@ -0,0 +1,9 @@
+<?php
+namespace App\Controllers;
+use CodeIgniter\API\ResponseTrait;
+use CodeIgniter\RESTful\ResourceController;
+
+class UsuariosController extends ResourceController{
+    use ResponseTrait;
+}
+?>
