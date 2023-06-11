90cbc0af5a0bae9d8b830813ad394ff5003f94e6
DavidEmmanuelPerezGarcia <106285896+DavidEmmanuelPerezGarcia@users.noreply.github.com>
2023-06-11 15:27:07 -0600

    Se creo el controlador de articulos

---
+++ app/Controllers/ArticulosController.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Controllers;
+use CodeIgniter\API\ResponseTrait;
+use CodeIgniter\RESTful\ResourceController;
+
+class ArticulosController extends ResourceController{
+    use ResponseTrait;
+
+    
+}
+
+?>
