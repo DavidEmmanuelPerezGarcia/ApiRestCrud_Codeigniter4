e07ed994e22bc8c48020143c27edf74356548e38
DavidEmmanuelPerezGarcia <106285896+DavidEmmanuelPerezGarcia@users.noreply.github.com>
2023-06-11 15:32:50 -0600

    Se agrego el metodo para traer los articulos

--- app/Controllers/ArticulosController.php
+++ app/Controllers/ArticulosController.php
@@ -1,12 +1,19 @@
 <?php
 namespace App\Controllers;
+
+use App\Models\ArticulosModel;
 use CodeIgniter\API\ResponseTrait;
 use CodeIgniter\RESTful\ResourceController;
 
 class ArticulosController extends ResourceController{
     use ResponseTrait;
 
-    
+    //metodo traer todos los articulos
+    public function index(){
+        $model=new  ArticulosModel();
+        $datos=$model->findAll();
+        return $this->respond($datos,200);
+    }
 }
 
 ?>
